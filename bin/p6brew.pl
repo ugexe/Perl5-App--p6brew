d2395e3a8bebffb370049dbcd3979ee89da80a9b
Nick Logan <ugexe@cpan.org>
2015-03-08 18:38:36 -0400

    init

---
+++ bin/p6brew.pl
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use feature 'say';
+use App::p6brew;
+
+my $app = App::p6brew->new_with_options;
+
---
+++ lib/App/p6brew.pm
@@ -0,0 +1,74 @@
+use Moops;
+use Moo;
+require MooX::Options;
+
+our $VERSION = 0.01;
+
+class Options {
+    role VM {
+        use MooX::Options;
+        option 'install_vm' => (
+            short       => 'vm',
+            is          => 'rw',
+            format      => 's@',
+            default     => sub {['moar']},
+            autosplit   => 1,
+            required    => 1,
+            doc         => 'VM backend(s) to install',
+        );
+    }
+
+
+    role Compiler {
+        use MooX::Options;
+        option 'install_compiler' => (
+            short       => 'c',
+            is          => 'rw',
+            format      => 's@',
+            default     => sub {['rakudo']},
+            autosplit   => 1,
+            required    => 0,
+            doc         => 'Perl6 compiler(s) to install',
+        );
+    }
+
+    role ModuleInstaller {
+        use MooX::Options;
+        option 'install_panda' => (
+            short       => 'panda',
+            is          => 'ro',
+            required    => 0,
+            doc         => 'Install panda, a module installer for Rakudo Perl6',
+        );
+    }
+} # class Options
+
+
+class App::p6brew with Options::VM with Options::Compiler with Options::ModuleInstaller {
+    use MooX::Options;
+    option verbose => (
+        is  => 'ro',
+        isa => Bool,
+    );
+
+    class Installer {
+        has $path => (
+            is  => "rw",
+            isa => Str,
+        );
+
+        method install() {
+            say "path:$path";
+            say "vm:" . $self->vm;
+        }
+    } # class Installer
+
+} # class App::p6brew
+
+
+
+1;
+
+
+
+__END__
